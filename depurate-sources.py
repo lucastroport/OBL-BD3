3d8e999c99eaad33777738f26db2446dc4e654b9
lucastroport <federicocastro999@gmail.com>
2023-05-27 17:32:43 -0300

    script for generating the sql files to replace 5555's and 88's values in viviendas, hogares and personas (personas still needs columns to add )

---
+++ depurate-sources.py
@@ -0,0 +1,33 @@
+import os
+
+def depure(directory):
+    fives = 5555
+    eights = 88
+    
+    hogares_table = "obligatorio.hogares"
+    viviendas_table = "obligatorio.viviendas"
+    personas_table = "obligatorio.personas"
+    
+    table_columns = {
+        hogares_table: ['NBI_EDUCAC', 'NBI_CANTID', 'NBI_HAC', 'NBI_CONFOR', 'NBI_VIV', 'HogCa01', 'HogSH01', 'HogSC01'],
+        viviendas_table: ['VivVO01', 'VivVO03', 'CATEVIV', 'VivDV05', 'VivDV06', 'VivDV01', 'VivDV03', 'VivDV02'],
+        personas_table: ['pobpcoac', 'PerDi01', 'PerDi05', 'PerDi02']
+    }
+    
+    result = ''
+    for table in table_columns:
+        for column in table_columns[table]:
+            result += f"UPDATE {table} SET {column} = NULL WHERE {column} = {fives} OR {column} = {eights};\n"
+        # Set the file path and name
+        file_path = os.path.join(directory, f"{table}_depurated.sql")
+        # Open the file in write mode
+        with open(file_path, 'w') as file:
+            file.write(result)
+            result = ''
+
+
+directory = "depurated_tables"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
+depure(directory)
