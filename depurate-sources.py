5649773ec4fa017e9a81622b4893237f960fef9b
Lucas Castro <federicocastro999@gmail.com>
2023-06-04 16:19:08 -0300

    fixed wrong column names

--- depurate-sources.py
+++ depurate-sources.py
@@ -11,7 +11,7 @@ def depure(directory):
     table_columns = {
         hogares_table: ['NBI_EDUCAC', 'NBI_CANTID', 'NBI_HAC', 'NBI_CONFOR', 'NBI_VIV', 'HogCa01', 'HogSH01', 'HogSC01', 'HogHD00', 'HogHD01', 'HogCa01', 'HogCE06', 'HogCE09', 'HogCE10', 'HogCE12', 'HogCE13'],
         viviendas_table: ['VivVO01', 'VivVO03', 'CATEVIV', 'VivDV05', 'VivDV06', 'VivDV01', 'VivDV03', 'VivDV02'],
-        personas_table: ['pobpcoac', 'PerDi01', 'PerDi05', 'PerDi02', 'PerPa01', 'PerMi02_1', 'PerMi05_1', 'PerEd03_r', 'PerEd03_1_r', 'PerEd03_2_r', 'PerEd05_r', 'PerEd06_r', 'PerFM01', 'PerFM01_1', 'PerFM02', 'PerFM02_1', 'PerFM04_2', 'niveledu_r', 'Años_estudio', 'PerFM01_r', 'PerFM02_r']
+        personas_table: ['pobpcoac', 'PerDi01', 'PerDi05', 'PerDi02', 'PerPa01', 'PerMi02_1', 'PerMi05_1', 'PerEd03_r', 'PerEd03_1', 'PerEd03_2', 'PerEd05_r', 'PerEd06_r', 'PerFM01', 'PerFM01_1', 'PerFM02', 'PerFM02_1', 'PerFM04_2', 'niveledu_r', 'v66', 'PerFM01_r', 'PerFM02_r']
     }
     
     result = ''
